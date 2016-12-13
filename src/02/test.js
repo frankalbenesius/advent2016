e8ef866d2535f582199c6668a958d56396708554
Frank Albenesius <frankalbenesius@gmail.com>
2016-12-12 22:27:39 -0600

    add solution to day 2: part 2

--- src/02/test.js
+++ src/02/test.js
@@ -5,6 +5,7 @@ import {
   toDirections,
   traverseKeyPad,
   toSquareBathroomCode,
+  // toDiamondBathroomCode,
 } from '.'
 
 test('parseInput', (t) => {
@@ -24,9 +25,16 @@ test('parseInput', (t) => {
 })
 
 test('toResult', (t) => {
-  t.test('returns an integer', (assert) => {
+  t.test('returns a function', (assert) => {
+    const input = 'square'
+    const actual = typeof toResult(input)
+    const expected = 'function'
+    assert.equal(actual, expected)
+    assert.end()
+  })
+  t.test('returns a function that returns an integer', (assert) => {
     const input = 'U'
-    const result = Number.isInteger(toResult(input))
+    const result = Number.isInteger(toResult('square')(input))
     assert.ok(result)
     assert.end()
   })
@@ -43,25 +51,32 @@ test('toDirections', (t) => {
 })
 
 test('traverseKeyPad', (t) => {
-  t.test('returns an integer', (assert) => {
+  t.test('returns a function', (assert) => {
+    const input = 'square'
+    const actual = typeof traverseKeyPad(input)
+    const expected = 'function'
+    assert.equal(actual, expected)
+    assert.end()
+  })
+  t.test('returns a function that returns an integer', (assert) => {
     const inputPosition = 5
     const inputDirection = 'U'
-    const result = traverseKeyPad(inputPosition, inputDirection)
+    const result = traverseKeyPad('square')(inputPosition, inputDirection)
     assert.ok(result)
     assert.end()
   })
-  t.test('returns 6 if position is 5 and direction is R', (assert) => {
+  t.test('returns a "square" fx that returns 6 if position is 5 and direction is R', (assert) => {
     const inputPosition = 5
     const inputDirection = 'R'
-    const actual = traverseKeyPad(inputPosition, inputDirection)
+    const actual = traverseKeyPad('square')(inputPosition, inputDirection)
     const expected = 6
     assert.equal(actual, expected)
     assert.end()
   })
-  t.test('returns 5 if position is 8 and direction is U', (assert) => {
+  t.test('returns a "square" fx that returns 5 if position is 8 and direction is U', (assert) => {
     const inputPosition = 8
     const inputDirection = 'U'
-    const actual = traverseKeyPad(inputPosition, inputDirection)
+    const actual = traverseKeyPad('square')(inputPosition, inputDirection)
     const expected = 5
     assert.equal(actual, expected)
     assert.end()
@@ -83,4 +98,21 @@ test('toSquareBathroomCode', (t) => {
     assert.equal(actual, expected)
     assert.end()
   })
+  t.test('day 2 example 1', (assert) => {
+    const input = 'ULL\nRRDDD\nLURDL\nUUUUD'
+    const actual = toSquareBathroomCode(input)
+    const expected = '1985'
+    assert.equal(actual, expected)
+    assert.end()
+  })
 })
+
+// test('toDiamondBathroomCode', (t) => {
+//   t.test('day 2 example 2', (assert) => {
+//     const input = 'ULL\nRRDDD\nLURDL\nUUUUD'
+//     const actual = toDiamondBathroomCode(input)
+//     const expected = '5DB3'
+//     assert.equal(actual, expected)
+//     assert.end()
+//   })
+// })
